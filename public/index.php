5a9c63d68c22afdb4ce6fc33314d5aaf99feb584
Iru21 <mateuszsamsel3@gmail.com>
2026-03-26 21:30:37 +0100

    Update storage logic

--- public/index.php
+++ public/index.php
@@ -1,14 +1,17 @@
 <?php
 
-if (!is_dir(__DIR__ . '/storage')) {
-    mkdir(__DIR__ . '/storage');
-}
-$files = scandir(__DIR__ . '/storage');
-$files = array_filter($files, function ($file) {
-    return !is_dir(__DIR__ . "/storage/$file") && $file !== '.' && $file !== '..';
-});
+$storagePath = __DIR__ . '/storage';
+$storageExists = is_dir($storagePath);
 
-$isStorageEmpty = count($files) === 0;
+if ($storageExists) {
+    $files = scandir($storagePath);
+    $files = array_filter($files, function ($file) use ($storagePath) {
+        return !is_dir($storagePath . "/$file") && $file !== '.' && $file !== '..';
+    });
+
+    $isStorageEmpty = count($files) === 0;
+}
+
 
 ?>
 
@@ -19,21 +22,25 @@ $isStorageEmpty = count($files) === 0;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>files.yuri.rocks</title>
 </head>
 <body>
-<!--    <ul>-->
-<!--        --><?php
-//            foreach ($files as $file) {
-//                echo "
-//                    <li>
-//                        <a target='_blank' href=\"storage/$file\">$file</a>
-//                    </li>";
-//            }
-//            if ($isStorageEmpty) {
-//                echo '<li>No files found</li>';
-//            }
-//        ?>
-<!--    </ul>-->
+<?php if (!$storageExists): ?>
+    <p>Storage directory does not exist.</p>
+<?php elseif (!$isStorageEmpty): ?>
+    <ul>
+        <?php
+        foreach ($files as $file) {
+            echo "
+                <li>
+                    <a target='_blank' href=\"storage/$file\">$file</a>
+                </li>
+            ";
+        }
+        ?>
+    </ul>
+<?php else: ?>
+    <p>The storage directory is empty.</p>
+<?php endif; ?>
 </body>
 </html>
